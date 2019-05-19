55991b893a24e16868a6a2f64c47bf7297d3e2d8
Ahmad Mabrouk <ahmad.mabrouk@plexable.com>
2019-05-20 00:18:34 +0300

    Initial Commit

---
+++ Ahmad_Mabrouk_Lamsa_Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ahmad_Mabrouk_Lamsa_Task
+//
+//  Created by Ahmad Mabrouk on 5/20/19.
+//  Copyright © 2019 Ahmad Mabrouk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ahmad_Mabrouk_Lamsa_TaskTests/Ahmad_Mabrouk_Lamsa_TaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ahmad_Mabrouk_Lamsa_TaskTests.swift
+//  Ahmad_Mabrouk_Lamsa_TaskTests
+//
+//  Created by Ahmad Mabrouk on 5/20/19.
+//  Copyright © 2019 Ahmad Mabrouk. All rights reserved.
+//
+
+import XCTest
+@testable import Ahmad_Mabrouk_Lamsa_Task
+
+class Ahmad_Mabrouk_Lamsa_TaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
